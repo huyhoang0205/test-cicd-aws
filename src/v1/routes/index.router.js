b7393b43972730cf716f4162c45175fce27982bb
Huy Hoang <cuocdoi69dep.nhulol@gmail.com>
2024-10-16 09:51:04 +0700

    FEAT:add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'huyhoang',
+                age: 22
+            },
+            {
+                name: 'huyhoang1',
+                age: 22
+            },
+            {
+                name: 'huyhoang2',
+                age: 22
+            }
+        ]
+    })
+})
+
 module.exports = router;
